8ac4d734841585986e0aa987d48742473293e6d4
QuanSenHuan <2542907805@qq.com>
2021-01-26 16:03:57 +0800

    测试

---
+++ src/nihao.java
@@ -0,0 +1,10 @@
+/**
+ * @Author:QuanSenHuan
+ * @Describe:
+ * @Date:Create in 16:01 2021/1/26
+ */
+public class nihao {
+    public static void main(String[] args) {
+        System.out.println("nihao");
+    }
+}
